c608106a0c09017e7aee60fb71b20090731b0f53
ytl001G <118708258+ytl001G@users.noreply.github.com>
2022-12-24 14:49:57 +0900

    [Gold V] Title: 노드사이의 거리, Time: 556 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,34 +1,51 @@
 #include <iostream>
+#include <algorithm>
 
 #define FASTIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define ll long long int
 #define FOR(i, a, b) for (int i = a; i < b; i++)
 #define FORR(i, a, b) for (int i = b-1; i > a-1; i--)
 #define iin(i) int i; cin >> i;
-#define loop(n) FOR(i,0,n)
+#define endl "\n"
 
 using namespace std;
+
+int n, m;
+
+int visited[1000];
+
+int graph[1000][1000];
+
+void dfs(int s, int e, int dist){
+    if (visited[s] == 1){
+        return;
+    }
+
+    if (s == e){
+        cout << dist << endl;
+        return;
+    }
+
+    visited[s] = 1;
+
+    FOR(i,0,n){
+        if (graph[s][i] != 0){
+            dfs(i, e, dist+graph[s][i]);
+        }
+    }
+}
 
 int main() {
     FASTIO;
 
-    iin(n);iin(m);
+    cin >> n >> m;
 
-    // 인접행렬
-    int **graph = new int *[n];
     FOR(i,0,n){
-        graph[i] = new int[n];
-
         FOR(j,0,n){
-            if (i == j){
-                graph[i][j] = 0;
-            } else {
-                graph[i][j] = 1<<20;
-            }
+            graph[i][j] = 0;
         }
     }
 
-    // input
     FOR(i,0,n-1){
         iin(x);iin(y);iin(c);
         x--;
@@ -37,24 +54,18 @@ int main() {
         graph[x][y] = graph[y][x] = c;
     }
 
-    // calc
-    FOR(mi,0,n){
-        FOR(s,0,n){
-            FOR(e,0,n){
-                if (graph[s][e] > graph[s][mi] + graph[mi][e]){
-                    graph[s][e] = graph[s][mi] + graph[mi][e];
-                }
-            }
-        }
-    }
-
     // solve
     FOR(i,0,m){
         iin(x);iin(y);
         x--;
         y--;
 
-        cout << graph[x][y] << "\n";
+        for (auto &&i : visited)
+        {
+            i = 0;
+        }
+
+        dfs(x, y, 0);
     }
 
     return 0;
